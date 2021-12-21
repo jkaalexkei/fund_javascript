6d9b8e04edcde07c11ddf54f8103fd2069d3f704
ALEXANDER GABRIEL VARELA <75146252+jkaalexkei@users.noreply.github.com>
2021-12-21 14:22:31 -0500

    Update operadoreslogicos.js

--- js/operadoreslogicos.js
+++ js/operadoreslogicos.js
@@ -5,7 +5,12 @@ operadores logicos
 && operador AND
 || operador OR
 ! operador de negacion o NOT
-?? operador mullish coalescing // fusion de nulos o union nulosa
+?? operador nullish coalescing // fusion de nulos o union nulosa
+
+
+20 && 10
+
+"hola" && ""
 
 
 
