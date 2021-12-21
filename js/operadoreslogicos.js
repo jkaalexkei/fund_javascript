af5f69f9635bd81ea27e49d0966693bd134b4d48
ALEXANDER GABRIEL VARELA <75146252+jkaalexkei@users.noreply.github.com>
2021-12-21 16:51:40 -0500

    Update operadoreslogicos.js

--- js/operadoreslogicos.js
+++ js/operadoreslogicos.js
@@ -2,11 +2,13 @@
 
 operadores logicos
 
-&& operador AND
-|| operador OR
+&& operador AND retorna el primer valora false. Cuando no encuentra un valor false retorna el ultimo valor verdadero que encuentre
+|| operador OR retorna un valor verdadero si al menos uno es verdadero y retorna false si ninguno es verdadero. en caso que ambas condiciones sean falsas retorna el ultimo false que encuentre
 ! operador de negacion o NOT
 ?? operador nullish coalescing // fusion de nulos o union nulosa
 
+sintaxis del operador ??
+condicion nula a evaluar ?? valor que retorna (fue diseñado para valores nulos)
 
 20 && 10
 
@@ -15,3 +17,13 @@ operadores logicos
 
 
 */
+
+/*operador and &&*/
+console.log( true && true) retorna true
+console.log( false && true) retorna false
+
+para que retorne verdadero ambas condiciones deben ser ciertas. Si alguna es false el resultado retornado será false
+
+/*operador || OR*/
+console.log( true || true) retorna true
+console.log( false || true) retorna true
