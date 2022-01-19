3b54a1acb67400b2c3dbdbf9e6914b88eaced8c3
ALEXANDER GABRIEL VARELA <75146252+jkaalexkei@users.noreply.github.com>
2022-01-19 12:01:32 -0500

    Update arrowfunctions.js

--- js/arrowfunctions.js
+++ js/arrowfunctions.js
@@ -1,6 +1,13 @@
 //arrow functions
 //en una sola linea se puede prescindir de las llaves
 //no es necesario asignarle un nombre
+//en una arrows function no es necesario colocar las llaves de apertura y cierre cuando la funcion es de una sola linea
+//las arrows function genera un retorno implicito
+//el nombre de la funcion proviene del nombre de la variable
+//un arrow function de una sola linea retorna el resultado de dicha linea
+
+
+
 let demo = ()=>{
   
     console.log('saludo')
@@ -21,15 +28,17 @@ let demo = ()=>{
   /*
   -tienen una sintaxis mas corta que la declaracion con function
   - heredan el valor de this del contexto en el que fueron creadas y no se reasigna
+  -arrow function no reasignan this
+  
   
   */
 
   let estudiante = {
     nombre: 'alex',
     saludar:()=> { console.log('hola soy '+ this.nombre); },
-    //arrows function adopta su valor del momento en el que es definido
+    //arrows function adopta su valor del contexto en el momento que es definido
     saludo: function(){
-        console.log('Hola soy '+ this.nombre)//aca el objeto el this adopta el valor en el momento que es ejecutado
+        console.log('Hola soy '+ this.nombre)//aca el objeto this adopta el valor en el momento que es ejecutado o ejecutada la funcion.
     }
 }
 
@@ -37,5 +46,17 @@ estudiante.saludar()//devuelve 'hola soy undefined'
 estudiante.saludo()//devuelve 'hola soy alex'
 
 
+//ejemplo2
 
+function Estudiante(){
+  this.nombre = 'alex'
+  this.saludar = () => console.log('hola ' + this.nombre);
+  
+}  
+
+let e = new Estudiante();
+e.saludar()
+  
+  
+  
   
